91b056bc193da69a640463dc1bb126c8aeb32b1b
pedroseabra1091 <pedroseabra1091@gmail.com>
2024-01-24 15:32:51 +0000

    Improvements to flash dismiss animation

--- app/javascript/controllers/dismiss_controller.js
+++ app/javascript/controllers/dismiss_controller.js
@@ -1,19 +1,22 @@
 import { Controller } from "@hotwired/stimulus"
 
-export default class extends Controller {
+export default class DismissController extends Controller {
   static targets = ['alert', 'notice']
 
   connect() {
-    this.removeAlert(this.noticeTarget, 5000)
+    this.dismiss(this.noticeTarget, 5000)
+    this.dismiss(this.alertTarget, 5000)
   }
 
   onClick(event) {
     event.preventDefault()
     const target = event.currentTarget
-    this.removeAlert(target, 200)
+    this.dismiss(target, 0)
   }
 
-  removeAlert(target, delay_ms) {
-    setTimeout(() => target.remove(), delay_ms)
+  dismiss(target, delay_ms) {
+    setTimeout(() => {
+      target.classList.add('transition', 'ease-linear', 'delay-75', 'opacity-0', 'translate-x-full', 'duration-300')
+    }, delay_ms)
   }
 }
