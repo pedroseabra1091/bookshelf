30d472ad7ae81f937de96d426d3dd098b2d37459
Artur Leite <73408965+ARTurleite6@users.noreply.github.com>
2024-03-05 17:07:17 +0000

    Fix bug that sends an email to every admin, instead of only sending to recipient (#1)

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -2,10 +2,8 @@ class AdminMailer < ApplicationMailer
   before_action :set_reserver
   before_action :set_book
 
-  default to: -> { User.where(is_admin: true).pluck(:email) }
-
   def new_reservation
-    mail(subject: "New reservation on #{@book.title}")
+    mail(subject: "New reservation on #{@book.title}", to: @reserver.email)
   end
 
   def reservation_ending
